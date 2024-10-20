ad24a686da618ed837417e8608426463d698e72a
beurmuz <fallinta2@gmail.com>
2024-10-20 23:08:44 +0900

    #1 [setting] 초기 세팅 - 폰트, icon 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,14 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <p>🔧 블로그 개발중 🔧</p>
+        <h1>h1 제목입니다</h1>
+        <h2>h2 제목입니다</h2>
+        <h3>h3 제목입니다</h3>
+        <div>
+          일단은 이렇게 만들어 보겠습니다..
+          <br />
+          오늘은 블로그를 만들어야 하거든요.
+        </div>
       </main>
       <footer className={styles.footer}>
         ©{nowYear}. beurmuz all rights reserved.
