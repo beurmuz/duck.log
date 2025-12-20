992998eb39919333322dbfb39c7d35da9fb3816d
beurmuz <fallinta2@gmail.com>
2025-12-20 09:51:42 +0900

    #15 [dev] notion data fetch & render

--- src/app/archive/[slug]/page.tsx
+++ src/app/archive/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 // import BackToLink from "@/components/UI/BackToLink";
 import { fetchNotionPostDetail } from "@/lib/notion/postDetail";
 import { fetchNotionPostList } from "@/lib/notion/extracts";
+import type { NotionPostList } from "@/models/post";
 import NotionRenderer from "@/components/NotionRenderer";
 import { formatDate } from "@/lib/dateUtils";
 import classNames from "classnames/bind";
@@ -9,53 +10,49 @@ import styles from "./ArchiveDetail.module.css";
 
 const cx = classNames.bind(styles);
 
-// 페이지 재검증 시간 7일로 설정
-export const revalidate = 604800;
+// 빌드 시 모든 포스트 페이지를 생성 (SSG)하되, 7일마다 포스트 페이지를 자동으로 재검증(ISR)하는 형태
 
-// 빌드 타임에 모든 slug를 미리 생성
-export async function generateStaticParams() {
+// ISR: 런타임) 페이지 재검증 시간 7일로 설정
+export const revalidate = 604800; // (= 7일)
+
+// SSG: 빌드 타임) [slug]의 모든 경로를 미리 생성해 각 페이지를 미리 생성
+export async function generateStaticPaths() {
   const posts = await fetchNotionPostList();
 
   return posts
-    .filter((post) => post.slug) // slug가 있는 포스트만
-    .map((post) => ({
-      slug: post.slug!,
-    }));
+    .filter((post): post is NotionPostList & { slug: string } => !!post.slug) // 전체 post에서 slug가 있는 것만 필터링
+    .map((post) => ({ slug: post.slug })); // slug만 추출하여 배열로 반환
 }
-
-type ArchiveDetailProps = {
-  params: Promise<{ slug: string }>;
-};
 
 export default async function ArchiveDetailPage({
   params,
-}: ArchiveDetailProps) {
+}: {
+  params: Promise<{ slug: string }>; // slug는 비동기 파라미터로 전달됨
+}) {
   const { slug } = await params;
 
   try {
-    const detail = await fetchNotionPostDetail(slug);
-
+    const postDetail = await fetchNotionPostDetail(slug);
     return (
       <section className={cx("wrap-page")}>
         <header className={cx("wrap-header")}>
-          {/* <BackToLink /> */}
-          {detail.categories.length > 0 && (
+          {postDetail.categories.length > 0 && (
             <ul className={cx("post-categories")}>
-              {detail.categories.map((category) => (
+              {postDetail.categories.map((category) => (
                 <li className={cx("post-category")} key={category}>
                   {category}
                 </li>
               ))}
             </ul>
           )}
-          <h1 className={cx("post-title")}>{detail.title}</h1>
+          <h1 className={cx("post-title")}>{postDetail.title}</h1>
           <div className={cx("post-date")}>
-            {formatDate(detail.updatedDate)} updated
+            {formatDate(postDetail.updatedDate)} updated
           </div>
         </header>
 
         <section className={cx("wrap-notioncontent")}>
-          <NotionRenderer blocks={detail.blocks} />
+          <NotionRenderer blocks={postDetail.blocks} />
         </section>
       </section>
     );
